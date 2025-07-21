073ffc6e68cf48f6cbe5a7e8dbf44a3c9e5f876b
nehad <Dark@DESKTOP-TQDVAO4>
2025-07-21 12:40:25 -0700

    1

--- Program.cs
+++ Program.cs
@@ -4,17 +4,19 @@
     {
         static void Main(string[] args)
         {
+            #region 1
             Person[] person01 = new Person[]
-                {
+                   {
                     new Person (20,"mohamed"),
                        new Person(27,"nehad"),
                   new Person(80,"mariam")
-                };
+                   };
 
             foreach (Person person in person01)
             {
                 Console.WriteLine($"the name is {person.name},and the age is {person.age}");
-            }
+            } 
+            #endregion
         }
     }
 }
