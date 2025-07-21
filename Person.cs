820db415246e3a1a6d66ef3526c06a2928660eca
nehad <Dark@DESKTOP-TQDVAO4>
2025-07-21 12:40:01 -0700

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssOOP2
+{
+    internal struct Person
+    {
+        public int age { get; set; }
+        public string name { get; set; }
+
+
+        public      Person(int _age,string _name)
+        {
+            age = _age;
+            name = _name;
+            
+        }
+
+
+        public override string ToString()
+        {
+            return $"name is {name} and the age is {age}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AssOOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] person01 = new Person[]
+                {
+                    new Person (20,"mohamed"),
+                       new Person(27,"nehad"),
+                  new Person(80,"mariam")
+                };
+
+            foreach (Person person in person01)
+            {
+                Console.WriteLine($"the name is {person.name},and the age is {person.age}");
+            }
+        }
+    }
+}
